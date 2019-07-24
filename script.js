76febd8ee93e1b2fc7b9a734c250642a40059063
Albozfootball <abloz_football@outlook.com>
2019-07-24 22:32:35 +0200

    added

---
+++ script.js
@@ -0,0 +1,89 @@
+const input = document.getElementById('userInput')
+const enter = document.getElementById('enter')
+const ul = document.getElementById('ul')
+
+
+
+const createListElement = () => {
+   const li = document.createElement('li')
+   li.appendChild(document.createTextNode(input.value))
+   ul.appendChild(li)
+   input.value = ''
+
+   const btn = document.createElement('button')
+   btn.appendChild(document.createTextNode('X'))
+   li.appendChild(btn)
+
+
+   const doneItem = () => {
+      li.classList.toggle('done')
+   }
+
+   const deleteItem = () => {
+      li.classList.add('delete')
+   }
+
+   li.addEventListener('click', doneItem)
+   btn.addEventListener('click', deleteItem)
+
+
+}
+
+const inputLength = () => {
+   return input.value.length
+}
+
+
+
+const addElementByKeypress = (event) => {
+   if (inputLength() > 0 && event.which === 13) {
+      createListElement()
+   }
+}
+
+const addElementByClick = () => {
+   if (inputLength() > 0 ) {
+      createListElement()
+   }
+}
+
+input.addEventListener('keypress', addElementByKeypress)
+enter.addEventListener('click', addElementByClick)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
